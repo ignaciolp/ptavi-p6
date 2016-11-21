d14fa949dc549eea28281deedbd5075942d26954
Ignacio Lopez Penalver <nacholp@gamma36.aulas.gsyc.urjc.es>
2016-11-21 12:04:00 +0100

    terminado

--- server.py
+++ server.py
@@ -14,11 +14,47 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     
+    
+    def handle(self):
+
+        # Escribe dirección y puerto del cliente (de tupla client_address)
+
+        #Leyendo linea a linea lo que nos envia el cliente
+        data = self.rfile.read().decode('utf-8')
+        METHOD = data.split(' ')[0]
+        
+      # Detección de método SIP
+        if METHOD == 'INVITE':
+            print("El cliente nos manda: " + data)
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+        elif METHOD == 'ACK':
+            print("El cliente nos manda: " + data)
+            aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + SONG
+            print("Executing...", aEjecutar)
+            print()
+            os.system(aEjecutar)
+
+        elif METHOD == 'BYE':
+            print("El cliente nos manda: " + data)
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+        elif METHOD not in ['INVITE', 'ACK', 'BYE']:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+        else:
+            self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+
+
+if __name__ == "__main__":
+    # Creamos servidor de eco y escuchamos
+
     IP = sys.argv[1]
-    PORT = (sys.argv[2])
+    PORT = int(sys.argv[2])
     SONG = sys.argv[3]
     print('IP: ' + IP)
-    print('PORT: ' + PORT)
+    print('PORT: ' + str(PORT))
 
     if len(sys.argv) != 4:
         sys.exit("Usage: python server.py IP port audio_file")
@@ -28,26 +64,11 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     else:
         print('Listening...')
 
-    def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-            elem = line.split(' ')
-            METHOD = elem[0]
-            if METHOD == 'INVITE':
-                self.wfile.write(b"SIP/2.0 100 Trying,SIP/2.0 180 Ring y SIP/2.0 200 OK\r\n\r\n")
-            if METHOD not in ['INVITE', 'ACK', 'BYE']:
-                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
-
-if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('',int(sys.argv[2]) ), EchoHandler)
+    serv = socketserver.UDPServer(('',PORT) , EchoHandler)
     print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print()
+        print("Finalizado servidor")
